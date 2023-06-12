354b58b2fb8035422c88d7a8ba8505668300ded9
Kian2002 <canadakurdistan@gmail.com>
2023-06-11 18:19:35 -0700

    add header to page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,17 @@
+import React from "react";
+
+import { Header } from "../components";
+
 export default function Home() {
-  return <div className="text-green-500">Main Content</div>;
+  return (
+    <div className="bg-neutral-900 rounded-lg w-full h-full overflow-hidden overflow-y-auto">
+      <Header>
+        <div className="mb-2">
+          <h1 className="text-white text-3xl font-semibold">Welcome Back</h1>
+
+          <div className="grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 gap-3 mt-4" />
+        </div>
+      </Header>
+    </div>
+  );
 }

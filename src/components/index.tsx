92332709fb3fe79f631afb4717eaae7a20d754be
Kian2002 <canadakurdistan@gmail.com>
2023-06-11 18:18:43 -0700

    export button & and header

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -2,5 +2,7 @@ import Sidebar from "./Sidebar";
 import SidebarItem from "./SidebarItem";
 import Box from "./Box";
 import Library from "./Library";
+import Header from "./Header";
+import Button from "./Button";
 
-export { Sidebar, Box, SidebarItem, Library };
+export { Sidebar, Box, SidebarItem, Library, Header, Button };

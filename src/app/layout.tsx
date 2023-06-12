a0c8c9837f2632fc2c9f08df90ca514799aa9f4a
Kian2002 <canadakurdistan@gmail.com>
2023-06-12 02:18:24 -0700

    update font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,12 @@
 import "./globals.css";
-import { Figtree } from "next/font/google";
+import { Montserrat } from "next/font/google";
 
 import { Sidebar } from "../components";
+import SupabaseProvider from "../providers/SupabaseProvider";
 
-const font = Figtree({ subsets: ["latin"] });
+const font = Montserrat({
+  subsets: ["latin"],
+});
 
 export const metadata = {
   title: "Spotify Clone",
@@ -18,7 +21,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={font.className}>
-        <Sidebar>{children}</Sidebar>
+        <SupabaseProvider>
+          <Sidebar>{children}</Sidebar>
+        </SupabaseProvider>
       </body>
     </html>
   );

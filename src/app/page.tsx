2965c81be486d6f1c3e330cbefed4745c2193f5c
Kian2002 <canadakurdistan@gmail.com>
2023-06-12 01:01:19 -0700

    add list item to header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-import { Header } from "../components";
+import { Header, ListItem } from "../components";
 
 export default function Home() {
   return (
@@ -9,9 +9,25 @@ export default function Home() {
         <div className="mb-2">
           <h1 className="text-white text-3xl font-semibold">Welcome Back</h1>
 
-          <div className="grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 gap-3 mt-4" />
+          <div className="grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 gap-3 mt-4">
+            <ListItem
+              image="/images/liked.png"
+              name="Liked Songs"
+              href="liked"
+            />
+          </div>
         </div>
       </Header>
+
+      <div className="mt-2 mb-7 px-6">
+        <div className="flex justify-between items-center">
+          <h1 className="text-white text-2xl font-semibold">Newest Songs</h1>
+        </div>
+
+        <div>
+          <h2>List of Songs</h2>
+        </div>
+      </div>
     </div>
   );
 }

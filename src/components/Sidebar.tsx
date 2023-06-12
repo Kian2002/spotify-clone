541057b4fc9adc3a036c20099ef6f35b8077dd0b
Kian2002 <canadakurdistan@gmail.com>
2023-06-11 18:19:16 -0700

    add classes to main

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -44,7 +44,7 @@ const Sidebar: React.FC<SidebarProps> = ({ children }) => {
           <Library />
         </Box>
       </div>
-      <main>{children}</main>
+      <main className="h-full flex-1 overflow-y-auto py-2">{children}</main>
     </div>
   );
 };

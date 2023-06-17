9a5e36124e6fa3eaa19090158b8fb9e76201a97a
Kian2002 <canadakurdistan@gmail.com>
2023-06-17 16:47:46 -0700

    import playButton

--- src/components/SongItem.tsx
+++ src/components/SongItem.tsx
@@ -4,6 +4,7 @@ import Image from "next/image";
 
 import useLoadImage from "@/hooks/useLoadImage";
 import { Song } from "../../types";
+import { PlayButton } from "../components";
 
 interface SongItemProps {
   song: Song;

6e4c21017361af51618106ab34cd68a195ec610f
Kian2002 <canadakurdistan@gmail.com>
2023-06-23 20:58:14 -0700

    add loading and error handling to pages

---
+++ src/app/Error.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import { Box } from "@/components";
+
+const Error = () => {
+  return (
+    <Box className="h-full flex items-center justify-center">
+      <h1 className="text-neutral-400">Something went wrong.</h1>
+    </Box>
+  );
+};
+
+export default Error;
